aab7b5e9a2b5b801ba80377639db3bbe7b1a929d
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2020-11-24 17:20:47 +0600

    wolves added

--- bot.js
+++ bot.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 const {Client} = require('discord.js');
 const bot = new Client();
-bot.on("ready", () =>{
-    console.log("Logged in")
-})
+
 bot.on("message", (message) =>{
     if (message.author.bot) return;
     const mentionedMembersCollection = message.mentions.members;
     const linux = mentionedMembersCollection.get("540140204909002752");
     if(mentionedMembersCollection.has('540140204909002752') || mentionedMembersCollection.has(bot.user.id)){
         message.reply(` Master ${linux} will catch up with you asap!`);
+    }
+});
+
+bot.on("message", (message) =>{
+    if (message.author.bot) return;
+    const mentionedMembersCollection = message.mentions.members;
+    const wolves = mentionedMembersCollection.get("535461330140921857");
+    if(mentionedMembersCollection.has('535461330140921857') || mentionedMembersCollection.has(bot.user.id)){
+        message.reply(` Master ${wolves} will catch up with you asap!`);
     }
 });
 bot.login(process.env.discord_bot_token);
