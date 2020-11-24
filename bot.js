eb304bff517cb7af27b057b1e9bcb95a1b59bff8
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2020-11-24 23:41:38 +0600

    fallen added

--- bot.js
+++ bot.js
@@ -16,8 +16,18 @@ bot.on("message", (message) =>{
     const mentionedMembersCollection = message.mentions.members;
     const wolves = mentionedMembersCollection.get("535461330140921857");
     if(mentionedMembersCollection.has('535461330140921857') || mentionedMembersCollection.has(bot.user.id)){
-        message.reply(` Master ${wolves} will catch up with you asap!`);
+        message.reply(`${wolves} will catch up with you asap!`);
     }
 });
+bot.on("message", (message) =>{
+    if (message.author.bot) return;
+    const mentionedMembersCollection = message.mentions.members;
+    const fallen = mentionedMembersCollection.get("610521079990976548");
+    if(mentionedMembersCollection.has('610521079990976548') || mentionedMembersCollection.has(bot.user.id)){
+        message.reply(`${fallen} will catch up with you asap!`);
+    }
+});
+
+
 bot.login(process.env.discord_bot_token);
 
